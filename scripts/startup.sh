9e52e26ea7e50d4ef8fe616a20bb5b54b7b71c57
Manikanta Talanki <manikanta.talanki@gmail.com>
2021-10-01 01:12:08 +0530

    silencing ping logging

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,4 +3,4 @@ cd /dynata/oauth2-proxy/contrib/$ENVIRONMENT/
 echo $ANSIBLE_SECRET > ansible-password.txt
 ansible-vault decrypt --vault-password-file ansible-password.txt oauth2-proxy-keycloak.cfg
 
-/bin/oauth2-proxy --config=/dynata/oauth2-proxy/contrib/$ENVIRONMENT/oauth2-proxy-keycloak.cfg
+/bin/oauth2-proxy --config=/dynata/oauth2-proxy/contrib/$ENVIRONMENT/oauth2-proxy-keycloak.cfg --silence-ping-logging
