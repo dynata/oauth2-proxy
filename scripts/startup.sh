023a14e8633785ae89f1754e8408e7e075489a05
Azfar Niaz <muhammadazfar@folio3.com>
2022-04-28 16:08:18 +0500

    - Fixing corpus client issue

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,11 +1,32 @@
-#!/bin/ash
-if [ -z "$ENVIRONMENT" ] then echo "ENVIRONMENT is unset"; else echo "ENVIRONMENT is set to '$ENVIRONMENT'"; fi
-if [ -z "$KC_HMAC_SECRET_KEY_HEX" ] then echo "KC_HMAC_SECRET_KEY_HEX is unset"; else echo "KC_HMAC_SECRET_KEY_HEX is set"; fi
-if [ -z "$KC_PRIVATE_KEY" ] then echo "KC_PRIVATE_KEY is unset"; else echo "KC_PRIVATE_KEY is set"; fi
-if [ -z "$DATABASE_HOST" ] then echo "DATABASE_HOST is unset"; else echo "DATABASE_HOST is set"; fi
-if [ -z "$DATABASE_PORT" ] then echo "DATABASE_PORT is unset"; else echo "DATABASE_PORT is set"; fi
-if [ -z "$DATABASE_USERNAME" ] then echo "DATABASE_USERNAME is unset"; else echo "DATABASE_USERNAME is set"; fi
-if [ -z "$DATABASE_PASSWORD" ] then echo "DATABASE_PASSWORD is unset"; else echo "DATABASE_PASSWORD is set"; fi
+#!/bin/bash
+if [ -z "$ENVIRONMENT" ] 
+then echo "ENVIRONMENT is unset" 
+else echo "ENVIRONMENT is set to '$ENVIRONMENT'" 
+fi
+if [ -z "$KC_HMAC_SECRET_KEY_HEX" ] 
+then echo "KC_HMAC_SECRET_KEY_HEX is unset" 
+else echo "KC_HMAC_SECRET_KEY_HEX is set" 
+fi
+if [ -z "$KC_PRIVATE_KEY" ] 
+then echo "KC_PRIVATE_KEY is unset" 
+else echo "KC_PRIVATE_KEY is set" 
+fi
+if [ -z "$DATABASE_HOST" ] 
+then echo "DATABASE_HOST is unset" 
+else echo "DATABASE_HOST is set" 
+fi
+if [ -z "$DATABASE_PORT" ] 
+then echo "DATABASE_PORT is unset" 
+else echo "DATABASE_PORT is set" 
+fi
+if [ -z "$DATABASE_USERNAME" ] 
+then echo "DATABASE_USERNAME is unset" 
+else echo "DATABASE_USERNAME is set" 
+fi
+if [ -z "$DATABASE_PASSWORD" ] 
+then echo "DATABASE_PASSWORD is unset" 
+else echo "DATABASE_PASSWORD is set" 
+fi
 
 ENV="${ENVIRONMENT:-local-environment}"
 
@@ -13,10 +34,11 @@ ENV="${ENVIRONMENT:-local-environment}"
 # ls -l
 # ./configKeycloakKeys.sh
 
-if [ "$ENV" == "local-environment" ]; then
+if [ "$ENV" == "local-environment" ] 
+then
   echo "executing local-environment environment configuration"
   # /bin/oauth2-proxy --config=/oauth2-proxy.cfg # mounted from docker-compose with correct configuration file
-  /bin/dlv --listen=:41800 --headless=true --api-version=2 --accept-multiclient exec /bin/oauth2-proxy --continue --config=/oauth2-proxy.cfg
+  /bin/dlv --listen=:41800 --headless=true --api-version=2 --accept-multiclient exec /bin/oauth2-proxy --continue -- --config=/oauth2-proxy.cfg
 else
   echo "executing non-local environment ($ENV) configuration"
   cd /dynata/oauth2-proxy/contrib/$ENVIRONMENT/
